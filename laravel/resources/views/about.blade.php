449ecae850954815153d1562168dd14b7e84020c
Eslamunto <eslam.shala@gmail.com>
2016-04-01 09:38:07 +0200

    undo backend files

--- laravel/resources/views/about.blade.php
+++
@@ -1,5 +0,0 @@
-<html>
-    <body>
-        <h1>Hello, I am the about page.</h1>
-    </body>
-</html>
--- laravel/resources/views/work.blade.php
+++
@@ -1,5 +0,0 @@
-<html>
-    <body>
-        <h1>Hello, I am the WORK page.</h1>
-    </body>
-</html>
