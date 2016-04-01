7e4954e716d88360e4b925366c5e8d5cf5c30729
Eslamunto <eslam.shala@gmail.com>
2016-04-01 15:11:38 +0200

    add models

---
+++ backend/app/Assignee.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Assignee extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'assignee';
+
+
+}
