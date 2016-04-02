99e4f84acf03e875282db679bdba7893fd79afa4
joseph wahba <joseph.s.wahba@gmail.com>
2016-04-02 22:46:48 +0200

    auth and role

--- backend/app/Http/routes.php
+++ backend/app/Http/routes.php
@@ -10,7 +10,37 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+Route::auth();
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome')->with(array('role'=>'guest'));
+});
+//
+Route::group(['middleware' => 'auth'], function () {
+
+
+    Route::group(['middleware' => 'admin'], function () {
+        Route::get('/admin', function () {
+            return view('welcome')->with(array('role'=>'admin'));
+        });
+
+    });
+
+
+
+    Route::group(['middleware' => 'supervisor'], function () {
+        Route::get('/supervisor', function () {
+            return view('welcome')->with(array('role'=>'supervisor'));
+        });
+
+    });
+
+
+    Route::group(['middleware' => 'agent'], function () {
+        Route::get('/agent', function () {
+            return view('welcome')->with(array('role'=>'agent'));
+        });
+
+    });
+
+    Route::get('/home', 'HomeController@index');
 });
