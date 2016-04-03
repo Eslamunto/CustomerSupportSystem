409c47c5e803497a16b9b2f08d456fd55487b866
narimanhesham <nariman.hesham93@gmail.com>
2016-04-03 13:30:22 +0200

    Testing authentication with the views

--- backend/resources/views/layouts/master.blade.php
+++ backend/resources/views/layouts/master.blade.php
@@ -71,7 +71,7 @@
                                     <a href="#" class="btn btn-default btn-flat">Profile</a>
                                 </div>
                                 <div class="pull-right">
-                                    <a href="#" class="btn btn-default btn-flat">Log out</a>
+                                    <a href="{{ URL('logout') }}" class="btn btn-default btn-flat">Log out</a>
                                 </div>
                             </li>
                         </ul>
