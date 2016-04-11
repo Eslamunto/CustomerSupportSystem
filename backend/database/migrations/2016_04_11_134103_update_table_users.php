4ae06c44e2a6f6f4a0256fc5a8df847fd608ecd2
narimanhesham <nariman.hesham93@gmail.com>
2016-04-11 19:45:43 +0200

    Updating Database Migrations and some Models

---
+++ backend/database/migrations/2016_04_11_134103_update_table_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('teamId')->nullable()->unsigned()->change();
+            $table->foreign('teamId')->references('id')->on('team')
+                ->onDelete('set null')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropForeign('users_teamid_foreign');
+        });
+    }
+}
---
+++ backend/database/migrations/2016_04_11_152539_add_department_id_foreign_key_to_table_team.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDepartmentIdForeignKeyToTableTeam extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('team', function (Blueprint $table) {
+            //
+            $table->integer('departmentId')->nullable()->unsigned()->change();
+            $table->foreign('departmentId')->references('id')->on('department')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('team', function (Blueprint $table) {
+            //
+            $table->dropForeign('team_departmentid_foreign');
+        });
+    }
+}
