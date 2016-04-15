9b34d1ebff56fa4bd23b2d9eb971382a2864a24a
Eslamunto <eslam.shala@gmail.com>
2016-04-15 19:51:49 +0200

    update index file for department

--- backend/resources/views/department/index.blade.php
+++ backend/resources/views/department/index.blade.php
@@ -1,74 +1,3 @@
-{{--<!-- app/views/nerds/index.blade.php -->--}}
-
-{{--<!DOCTYPE html>--}}
-{{--<html>--}}
-{{--<head>--}}
-    {{--<title>Look! I'm CRUDding</title>--}}
-    {{--<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">--}}
-{{--</head>--}}
-{{--<body>--}}
-{{--<div class="container">--}}
-
-    {{--<h1>All the Departments</h1>--}}
-
-
-    {{--<!-- will be used to show any messages -->--}}
-    {{--@if (Session::has('message'))--}}
-    {{--<div class="alert alert-info">{{ Session::get('message') }}</div>--}}
-    {{--@endif--}}
-
-    {{--<table class="table table-striped table-bordered">--}}
-    {{--<thead>--}}
-    {{--<tr>--}}
-    {{--<td>ID</td>--}}
-    {{--<td>Name</td>--}}
-    {{--<td>Email</td>--}}
-    {{--<td>Nerd Level</td>--}}
-    {{--<td>Actions</td>--}}
-    {{--</tr>--}}
-    {{--</thead>--}}
-    {{--<tbody>--}}
-    {{--@foreach($departments as $key => $value)--}}
-        {{--<ul>--}}
-            {{--<li>{{ $value->id }}</li>--}}
-            {{--<li>{{ $value->name }}</li>--}}
-
-            {{--{!! Form::open(array('url' => 'department/' . $value->id)) !!}--}}
-            {{--//<input type="hidden" formmethod="DELETE">--}}
-            {{--{!! Form::hidden('_method', 'DELETE') !!}--}}
-
-            {{--{!! Form::submit('Delete Me!', array('class' => 'btn btn-warning')) !!}--}}
-            {{--{!! Form::close() !!}--}}
-
-
-            {{--<td>{{ $value->nerd_level }}</td>--}}
-
-            {{--<!-- we will also add show, edit, and delete buttons -->--}}
-            {{--<td>--}}
-
-            {{--<!-- delete the nerd (uses the destroy method DESTROY /nerds/{id} -->--}}
-            {{--<!-- we will add this later since its a little more complicated than the other two buttons -->--}}
-
-            {{--<!-- show the nerd (uses the show method found at GET /nerds/{id} -->--}}
-            {{--<a class="btn btn-small btn-success" href="{{ URL::to('nerds/' . $value->id) }}">Show this Nerd</a>--}}
-
-            {{--<!-- edit this nerd (uses the edit method found at GET /nerds/{id}/edit -->--}}
-            {{--<a class="btn btn-small btn-info" href="{{ URL::to('nerds/' . $value->id . '/edit') }}">Edit this Nerd</a>--}}
-
-            {{--</td>--}}
-        {{--</ul>--}}
-    {{--@endforeach--}}
-    {{--</tbody>--}}
-    {{--</table>--}}
-
-{{--</div>--}}
-{{--</body>--}}
-{{--</html>--}}
-
-
-
-
-
 @extends('layouts.master')
 
 @section('title')
@@ -97,20 +26,14 @@
                     </thead>
                     <tbody>
                     @foreach($departments as $key => $value)
-                            <li>{{ $value->id }}</li>
-
-
-
-
-
                     <tr>
                         <td>{{ $value->name }}</td>
                         <td>
-                            <a href="#" data-toggle="modal" data-target="#editDepartment-{{ $value->id }}">Edit</a> | {!! Form::open(array('url' => 'department/' . $value->id)) !!}
+                            <a href="#" data-toggle="modal" data-target="#editDepartment-{{ $value->id }}">Edit</a> | {!! Form::open(array('url' => 'department/' . $value->id, 'class' => 'department-delete-form')) !!}
                             {{--<input type="hidden" formmethod="DELETE">--}}
                             {!! Form::hidden('_method', 'DELETE') !!}
 
-                            {!! Form::submit('Delete', array('class' => 'btn')) !!}
+                            {!! Form::submit('Delete', array('class' => 'department-delete-button')) !!}
                             {!! Form::close() !!}
 
                             <!-- Edit customer modal -->
@@ -139,22 +62,10 @@
 
 
                                                         <button type="submit" class="btn btn-default">
-                                                            <i class="fa fa-plus"></i> Edit Task
+                                                            <i class="fa fa-plus"></i> Edit Department
                                                         </button>
                                                     {!! Form::close() !!}
 
-
-
-                                                    {{--<!-- form start -->--}}
-                                                    {{--<form role="form">--}}
-                                                        {{--<div class="form-group">--}}
-                                                            {{--<label for="CustomerName">Department Name</label>--}}
-                                                            {{--<input type="text" class="form-control" id="name" placeholder="Customer Name">--}}
-                                                        {{--</div>--}}
-                                                        {{--<button type="submit" class="btn btn-primary bg-blue pull-right">--}}
-                                                            {{--<i class="fa fa-edit"></i> Save Changes--}}
-                                                        {{--</button>--}}
-                                                    {{--</form>--}}
                                                 </div><!-- /.box-body -->
                                             </div><!-- /.box -->
                                         </div>
@@ -198,41 +109,6 @@
 
             </ul>
 
-
-
-
-            <!-- Ticket Priority -->
-            {{--<div class="box box-solid">--}}
-            {{--<div class="box-header with-border">--}}
-            {{--<h4 class="box-title">Priority</h4>--}}
-            {{--</div>--}}
-            {{--<div class="box-body">--}}
-            {{--<!-- the events -->--}}
-            {{--<div id="external-events">--}}
-            {{--<div class="external-event bg-green ui-draggable ui-draggable-handle" style="position: relative;">--}}
-            {{--One--}}
-            {{--</div>--}}
-            {{--<div class="external-event bg-yellow ui-draggable ui-draggable-handle" style="position: relative;">--}}
-            {{--Two--}}
-            {{--</div>--}}
-            {{--<div class="external-event bg-aqua ui-draggable ui-draggable-handle" style="position: relative;">--}}
-            {{--Three--}}
-            {{--</div>--}}
-            {{--<div class="external-event bg-light-blue ui-draggable ui-draggable-handle" style="position: relative;">--}}
-            {{--Four--}}
-            {{--</div>--}}
-            {{--<div class="external-event bg-red ui-draggable ui-draggable-handle" style="position: relative;">--}}
-            {{--Paid--}}
-            {{--</div>--}}
-            {{--<div class="input-group">--}}
-            {{--<input id="new-event" type="text" class="form-control" placeholder="Create New Priority">--}}
-            {{--<div class="input-group-btn">--}}
-            {{--<button id="add-new-event" type="button" class="btn bg-blue btn-flat">Add</button>--}}
-            {{--</div><!-- /btn-group -->--}}
-            {{--</div>--}}
-            {{--</div>--}}
-            {{--</div><!-- /.box-body -->--}}
-            {{--</div><!-- /. box -->--}}
     </div>
 
 @endsection
