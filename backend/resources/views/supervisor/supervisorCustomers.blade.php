c1ca3d77422c4157b938db9f190d8d19f67d8203
Mahmoud Eldesouky <mahmoud@eldesouky.com>
2016-04-21 23:38:51 +0200

    adding dismiss

--- backend/resources/views/supervisor/supervisorCustomers.blade.php
+++ backend/resources/views/supervisor/supervisorCustomers.blade.php
@@ -11,11 +11,12 @@
             {{ Session::get('message') }}
         </div>
     @endif
-    @if (count($errors) > 0)
-        <div class="alert alert-danger">
+    @if($errors->any())
+        <div class="alert alert-danger alert-dismissable">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
             <h4> Please Fix The Following Error(s)</h4>
             <ul>
-                @foreach ($errors->all() as $error)
+                @foreach($errors->all() as $error)
                     <li>{{ $error }}</li>
                 @endforeach
             </ul>
