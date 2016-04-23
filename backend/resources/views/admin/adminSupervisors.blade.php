057f26bf6ae2b6cf790877bfee732e1aed62c751
joseph wahba <joseph.s.wahba@gmail.com>
2016-04-23 23:08:54 +0200

    Twitter feed from a single account done for admin and supervisor. Create ticket all helpers done only the main query I am still working on it

--- backend/resources/views/admin/adminSupervisors.blade.php
+++ backend/resources/views/admin/adminSupervisors.blade.php
@@ -49,19 +49,6 @@
                         <td>{{App\Department::find(App\Team::find($supervisors->teamId)->departmentId)->name}}</td>
                         <td>{{App\Team::find($supervisors->teamId)->name}}</td>
                         <td>{{count(App\UserTicket::where('userId','=',$supervisors->id)->get())}}</td>
-                        <script>
-                            var supervisorId = 0 ;
-                            function setSupervisorId(id){
-                                agentId = id;
-                                route = '../show/supervisor/' + id;
-
-                                $.getJSON(route, function(supervisor){
-                                    $('#editName').val(supervisor.name);
-                                    $('#editEmail').val(supervisor.email);
-                                });
-                                $('#editForm').attr("action", "../edit/supervisor/"+agentId);
-                            }
-                        </script>
                         <td>
                             <a href="#" onclick="setSupervisorId({{$supervisors->id}})" data-toggle="modal" data-target="#adminEditSupervisor">Edit</a> | <a data-method='delete' data-token="{{ csrf_token() }}" href="{{route('deleteSupervisor', $supervisors->id)}} "  data-confirm="Are you sure you want to delete '{{ $supervisors->name }}' ?">Delete</a>
                         </td>
@@ -88,60 +75,9 @@
     {{ HTML::script('plugins/fastclick/fastclick.min.js') }}
             <!-- page script -->
     <script>
-        $(function () {
-            $("#systemSupervisors").DataTable();
-        });
+        var department = "{{url('admin/department/')}}";
+        var edit = "{{url('/admin/edit/supervisor/')}}";
+        var show = "{{url('admin/show/supervisor')}}";
     </script>
-    <script>
-        window.onload = function () {
-            $('[data-method]').click(function(e) {
-                e.preventDefault();
-                // If the user confirm the delete
-                if (confirm($(this).data('confirm'))) {
-                    // Get the route URL
-                    var url = $(this).prop('href');
-                    // Get the token
-                    var token = $(this).data('token');
-                    //Get the method type
-                    var method = $(this).data('method');
-                    // Create a form element
-                    var $form = $('<form/>', {action: url, method: 'post'});
-                    // Add the DELETE hidden input method
-                    var $inputMethod = $('<input/>', {type: 'hidden', name: '_method', value: method});
-                    // Add the token hidden input
-                    var $inputToken = $('<input/>', {type: 'hidden', name: '_token', value: token});
-                    // Append the inputs to the form, hide the form, append the form to the <body>, SUBMIT !
-                    $form.append($inputMethod, $inputToken).hide().appendTo('body').submit();
-                }
-            });
-
-            $('#teams').hide();
-            $('#editTeams').hide();
-            $('#editDepartments').change(function () {
-                $('#teamsOptionsEdit').empty();
-                var id = $('#editDepartments').val();
-                var route = '../../admin/department/' + id + '/teams';
-                $.getJSON(route, function (json) {
-                    $.each(json, function(i, field){
-                        $('#teamsOptionsEdit').append("<option value='"+ field.id +"'>" + field.name + "</option>");
-                    });
-                    $('#editTeams').show();
-                });
-
-            });
-            $('#departments').change(function () {
-                $('#teamsOptions').empty();
-                var id = $('#departments').val();
-                var route = '../../admin/department/' + id + '/teams';
-                $.getJSON(route, function (json) {
-                    $.each(json, function(i, field){
-                        $('#teamsOptions').append("<option value='"+ field.id +"'>" + field.name + "</option>");
-                    });
-                    $('#teams').show();
-                });
-
-            });
-
-        }
-    </script>
+    {{ HTML::script('dist/js/adminSupervisors.js') }}
 @endsection
